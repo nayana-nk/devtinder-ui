996433ac012141a9a5f0851223e6aaf198416f77
Nayana <nayanank.23@gmail.com>
2025-05-19 12:04:10 +0530

    request page changes

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit"
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
 import connectionReducer from "./connectionSlice"
+import requestReudcer from "./requestsSlice"
 const appStore = configureStore({
 
     reducer:{
         user: userReducer,
         feed:  feedReducer,
         connections: connectionReducer,
+        requests:requestReudcer
     },
 })
 export default appStore;
