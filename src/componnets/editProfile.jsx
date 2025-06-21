5298d95f2bb00ee3cf17f85cddf9824aa5cedebb
Nayana <nayanank.23@gmail.com>
2025-06-21 15:18:34 +0530

    css changes

--- src/componnets/editProfile.jsx
+++ src/componnets/editProfile.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import  "./profile.css"
 
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
@@ -44,20 +45,20 @@ const EditProfile = ({ user }) => {
 
   return (
     <>
-      <div className="d-block d-md-flex justify-center my-10">
-        <div className="d-block d-md-flex justify-center mx-10">
-          <div className="card bg-base-300  shadow-xl">
+      <div className=" editProfile justify-center my-10">
+        <div className="flex justify-center mx-10">
+          <div className="card bg-base-300 w-96 shadow-xl">
             <div className="card-body">
               <h2 className="card-title justify-center">Edit Profile</h2>
               <div>
-                <label className="form-control w-full max-w-xs my-3">
+                <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
                     <span className="label-text">First Name:</span>
                   </div>
                   <input
                     type="text"
                     value={firstName}
-                    className="input input-bordered w-full max-w-xs my-2"
+                    className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setFirstName(e.target.value)}
                   />
                 </label>
@@ -69,7 +70,7 @@ const EditProfile = ({ user }) => {
                     <input
                       type="text"
                       value={lastName}
-                      className="input input-bordered w-full max-w-xs my-2"
+                      className="input input-bordered w-full max-w-xs"
                       onChange={(e) => setLastName(e.target.value)}
                     />
                   </label>
@@ -79,7 +80,7 @@ const EditProfile = ({ user }) => {
                   <input
                     type="text"
                     value={photoUrl}
-                    className="input input-bordered w-full max-w-xs my-2"
+                    className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setPhotoUrl(e.target.value)}
                   />
                 </label>
@@ -90,7 +91,7 @@ const EditProfile = ({ user }) => {
                   <input
                     type="text"
                     value={age}
-                    className="input input-bordered w-full max-w-xs my-2"
+                    className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setAge(e.target.value)}
                   />
                 </label>
@@ -101,7 +102,7 @@ const EditProfile = ({ user }) => {
                   <input
                     type="text"
                     value={gender}
-                    className="input input-bordered w-full max-w-xs my-2"
+                    className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setGender(e.target.value)}
                   />
                 </label>
@@ -112,7 +113,7 @@ const EditProfile = ({ user }) => {
                   <input
                     type="text"
                     value={about}
-                    className="input input-bordered w-full max-w-xs my-2"
+                    className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setAbout(e.target.value)}
                   />
                 </label>
